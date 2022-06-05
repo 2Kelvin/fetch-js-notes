efcb9f8c3e03328609d25db9873c0215f485a554
2Kelvin <kvin.mwangi254@gmail.com>
2022-06-05 10:28:53 +0300

    learning fetch API

--- notes.js
+++ notes.js
@@ -10,7 +10,7 @@
     // promises are better; they were invented to solve this headache
 
 // PROMISES
-    // has 3 states: "loading/pending", "successful/fulfilled" & "error/rejected"
+    // has 3 states: "loading(pending)", "successful/(fulfilled)" & "error/(rejected)"
     // it's asynchronous
     // example: btw you can make a new promise object through the promise constructor
     const myPromise = new Promise((resolve, reject) => {
@@ -23,15 +23,44 @@
     });
 
     console.log(myPromise); //logs "Yes. The promise has been resolved" since the if statement 'error = false' is true
-    // the promise above doesn't display the 'direct' value exactly, hence the evolution of thenables
+    // the promise above doesn't display the value, it displays the state of the promise hence the evolution of thenables
 
 // THENABLES
-    // it's basically a chained promise
+    // it's basically a chained promise but unlike a promise, it gives you the value to work with
     myPromise.then(value => {
         return value + 1;
     }).then(newValue => {
         console.log(newValue);
-    }).catch(err => {
+    }).catch(err => { //catch is responsible for catching the error in the code and dispalying it
         consolelog(err);
     })
-    // 
+    
+    mySecondPromise = new Promise((resolve, reject) => {
+        setTimeout(function () { 
+            resolve("myNextPromise resolved"); 
+        }, 3000);
+    });
+
+    mySecondPromise.then(value => {
+        console.log(value);
+    });
+    myPromise.then(value => {
+        console.log(value);
+    });
+    // 'myPromise' will output first before 'mySecondPromise' because it has a delay of 3 seconds
+
+
+// FETCH API
+    // fetch has an inbuilt promise in it : fetch automatically returns a promise
+    // fetch requests data from another server
+    // example:
+    const users = fetch("https://jsonplaceholder.typicode.com/users");
+    console.log(users); //here you'll get a pending state; as the fetch retrieves the data to your site/app
+    
+    // using fetch with thenables
+fetch("https://jsonplaceholder.typicode.com/users")
+    .then(Response => {
+        return Response.json();
+    }).then(data => {
+        console.log(data);
+    })
